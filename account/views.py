7d024e22570cb5a3500d7d1df7817f36fdc87050
helloworld575 <zhichenli6@gmail.com>
2017-11-28 23:57:00 +0800

    no change

--- account/views.py
+++ account/views.py
@@ -98,5 +98,10 @@ def myself_edit(request):
 
 @login_required(login_url='/account/login/')
 def my_image(request):
-
-    return render(request, 'account/imagecrop.html',)
+    if request.method=='POST':
+        img=request.POST['img']
+        userinfo=UserInfo.objects.get(user=request.user.id)
+        userinfo.save()
+        return HttpResponse("1")
+    else:
+        56return render(request, 'account/imagecrop.html',)
