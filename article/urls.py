63155580b140cad7d18a85024d935ed944ae7c15
helloworld575 <zhichenli6@gmail.com>
2017-12-02 19:43:57 +0800

    add article app and article edit and delete and classify.use https://github.com/pandao/editor.md help edit blog

--- article/urls.py
+++ article/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns=[
     url(r'^article-column/$', views.article_column, name="article_column"),
+    url(r'^del-column/$',views.del_column,name="del_column"),
+    url(r'^rename-column/$',views.rename_column,name="rename_column"),
+    url(r'^article-post/$',views.article_post,name="article_post"),
+    url(r'^article-list/$',views.article_list,name="article_list"),
+    url(r'^article-detail/(?P<article_id>\d+)/(?P<slug>[-\w]+)/$',views.article_detail,name="article_detail"),
+    url(r'^del-article/$',views.del_article,name="del_article"),
+    url(r'^edit-article/(?P<article_id>\d+)/$',views.edit_article,name="redit_article"),
 ]
--- article/views.py
+++ article/views.py
@@ -1,8 +1,118 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import ArticleColumn
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+
+
+from .forms import ArticleColumnForm,ArticlePostForm
+from .models import ArticleColumn,ArticlePost
+
 
 @login_required(login_url='/account/login/')
+@csrf_exempt
 def article_column(request):
-    columns=ArticleColumn.objects.filter(user=request.user)
-    return render(request,"article/column/article_column.html")
+    if request.method=="GET":
+        columns=ArticleColumn.objects.filter(user=request.user)
+        column_form=ArticleColumnForm()
+        return render(request,"article/column/article_column.html",{"columns":columns,"column_form":column_form})
+    elif request.method=="POST":
+        column_name=request.POST['column']
+        columns=ArticleColumn.objects.filter(user_id=request.user.id,column=column_name)
+        if columns:
+            return HttpResponse('2')
+        else:
+            ArticleColumn.objects.create(user=request.user,column=column_name)
+            return HttpResponse('1')
+
+@login_required(login_url='/account/login/')
+@csrf_exempt
+@require_POST
+def rename_column(request):
+    column_name=request.POST['column_name']
+    column_id=request.POST['column_id']
+    try:
+        line=ArticleColumn.objects.get(id=column_id)
+        line.column=column_name
+        line.save()
+        return HttpResponse("1")
+    except:
+        return HttpResponse("0")
+
+@login_required(login_url='/account/login/')
+@csrf_exempt
+@require_POST
+def del_column(request):
+    column_id=request.POST['column_id']
+    try:
+        line=ArticleColumn.objects.get(id=column_id)
+        line.delete()
+        return HttpResponse("1")
+    except:
+        return HttpResponse("2")
+
+@login_required(login_url='/account/login')
+@csrf_exempt
+def article_post(request):
+    if request.method=="POST":
+        article_post_form=ArticlePostForm(request.POST)
+        if article_post_form.is_valid():
+            cd = article_post_form.cleaned_data
+            try:
+                new_article=article_post_form.save(commit=False)
+                new_article.author=request.user
+                new_article.column=request.user.article_column.get(id=request.POST['column_id'])
+                new_article.save()
+                return HttpResponse("1")
+            except:
+                return HttpResponse("2")
+        else:
+            return HttpResponse("3")
+    else:
+        article_post_form=ArticlePostForm()
+        article_column=request.user.article_column.all()
+        return render(request,"article/column/article_post.html",{"article_post_form":article_post_form,"article_column":article_column})
+
+@login_required(login_url='/account/login')
+def article_list(request):
+    articles=ArticlePost.objects.filter(author=request.user)
+    return render(request, 'article/column/article_list.html',{"articles":articles})
+
+@login_required(login_url='/account/login')
+def article_detail(request,article_id,slug):
+    article=get_object_or_404(ArticlePost,id=article_id,slug=slug)
+    return render(request,'article/column/article_detail.html',{"article":article})
+
+@login_required(login_url='/account/login')
+@csrf_exempt
+@require_POST
+def del_article(request):
+    article_id=request.POST["article_id"]
+    try:
+        article=ArticlePost.objects.get(id=article_id)
+        article.delete()
+        return HttpResponse("1")
+    except:
+        return HttpResponse("2")
+
+
+@login_required(login_url='/account/login')
+@csrf_exempt
+def edit_article(request,article_id):
+    if request.method=="GET":
+        article_columns=request.user.article_column.all()
+        # article=ArticlePost.objects.get(id=article_id)
+        article=get_object_or_404(ArticlePost,id=article_id)
+        this_article_form=ArticlePostForm(initial={"title":article.title})
+        this_article_column=article.column
+        return render(request,'article/column/redit_article.html',{"article":article,"article_columns":article_columns,"this_article_form":this_article_form,"this_article_column":this_article_column})
+    else:
+        redit_article=ArticlePost.objects.get(id=article_id)
+        try:
+            redit_article.column=request.user.article_column.get(id=request.POST['column_id'])
+            redit_article.title=request.POST['title']
+            redit_article.body=request.POST['body']
+            redit_article.save()
+            return HttpResponse("1")
+        except:
+            return HttpResponse("2")
